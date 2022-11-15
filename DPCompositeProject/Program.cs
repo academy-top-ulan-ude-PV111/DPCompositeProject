8fb5ade15029842f94f78bcdef1063dcd531d10a
efimov-top-academy <efimov_m@t.top-academy.ru>
2022-11-15 16:25:16 +0300

    Добавьте файлы проекта.

---
+++ DPCompositeProject/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace DPCompositeProject
+{
+    internal class Program
+    {
+        static Unit CreateUnit(int number)
+        {
+            switch(number)
+            {
+                case 0: return new Infantry();
+                case 1: return new Archer();
+                case 2: return new Cavalery();
+                default:
+                    return null;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Random random = new();
+
+            Team army = new Team();
+
+            Team team1 = new Team();
+
+            for (int i = 0; i < 10; i++)
+                team1.Units.Add(CreateUnit(random.Next(0, 2)));
+
+            for (int i = 0; i < 10; i++)
+                army.Units.Add(CreateUnit(random.Next(0, 2)));
+
+            army.Units.Add(team1);
+
+            army.Info();
+        }
+    }
+}
---
+++ DPCompositeProject/Unit.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPCompositeProject
+{
+    abstract class Unit
+    {
+        public abstract void Info();
+        public abstract int Attack();
+    }
+
+    class Infantry : Unit
+    {
+        public override void Info()
+        {
+            Console.WriteLine("Infantry");
+        }
+
+        public override int Attack()
+        {
+            return 1;
+        }
+    }
+
+    class Cavalery : Unit
+    {
+        public override void Info()
+        {
+            Console.WriteLine("Cavalery");
+        }
+
+        public override int Attack()
+        {
+            return 3;
+        }
+    }
+
+    class Archer : Unit
+    {
+        public override void Info()
+        {
+            Console.WriteLine("Archer");
+        }
+
+        public override int Attack()
+        {
+            return 2;
+        }
+    }
+
+    class Team : Unit
+    {
+        public List<Unit> Units { set; get; } = new List<Unit>();
+        public override int Attack()
+        {
+            int attackFull = 0;
+            foreach (Unit unit in Units)
+                attackFull += unit.Attack();
+            return attackFull;
+        }
+
+        public override void Info()
+        {
+            Console.WriteLine("\tTeam units");
+            foreach(Unit unit in Units)
+            {
+                Console.Write("\t");
+                unit.Info();
+            }
+               
+        }
+    }
+}
